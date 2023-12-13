81d70b7cc4453f9fea517e2c7cba55dd2813eaea
Auc7us <keshavsharan97@gmail.com>
2023-12-12 22:29:06 -0600

    add meta leanring

---
+++ research/vid2depth/MAML.py
@@ -0,0 +1,139 @@
+"""
+Example Use:
+
+python MAML.py \
+  --data_dir /home/flash/vid2depth/data/bike \
+  --num_tasks 4 \
+  --num_inner_updates 3 \
+  --inner_lr 0.0002 \
+  --meta_lr 0.001 \
+  --num_epochs 3
+
+  """
+
+
+import tensorflow as tf
+import os
+from absl import app
+from absl import flags
+from absl import logging
+from trainMaml import vid2depth
+
+FLAGS = flags.FLAGS
+flags.DEFINE_string('data_dir', '/home/flash//vid2depth/data/bike', 'Directory where tasks data is stored.')
+flags.DEFINE_integer('num_tasks', 5, 'Number of tasks to train on.')
+flags.DEFINE_integer('num_inner_updates', 2, 'Number of updates in the inner loop.')
+flags.DEFINE_float('inner_lr', 0.0002, 'Learning rate for the inner loop.')
+flags.DEFINE_float('meta_lr', 0.001, 'Learning rate for the meta-update.')
+flags.DEFINE_integer('num_epochs', 2, 'Number of epochs to train.')
+
+def maml_inner_loop(support_set_dir, weights, epoch_num, num_inner_updates, inner_lr):
+    """
+    Executes the MAML inner loop.
+    Args:
+    - support_set_dir: The directory of the support set.
+    - weights: A list of weights for the model
+    - epoch_num: The number of the current epoch
+    - num_inner_updates: The number of epochs fow which the inner loop must train on the support data
+    - inner_lr: The learning rate for the inner loop updates.
+    
+    Returns:
+    - updated_weights: The model weights after inner loop updates.
+    - support_set_loss: The support set loss after the final inner update.
+    """
+    # Calculate the loss on the support set and perform the gradient updates
+    support_set_loss, support_set_gradients = vid2depth(support_set_dir, True, weights[0], weights[1], weights[2], num_inner_updates, epoch_num)
+    updated_weights = [
+    tf.assign_sub(weights[0], inner_lr * support_set_gradients[0]),
+    tf.assign_sub(weights[1], inner_lr * support_set_gradients[1]),
+    tf.assign_sub(weights[2], inner_lr * support_set_gradients[2])
+]
+    # Return the weights after the inner loop updates and the final support set loss
+    return updated_weights, support_set_loss
+
+
+def maml_outer_loop(tasks, weights, num_inner_updates, epoch_num, inner_lr, meta_lr, num_tasks):
+    """
+    Executes the MAML outer loop, which includes the meta-update step.
+
+    Args:
+    - tasks: A list of tasks, each task containing a support set and a query set
+    - weights: A list of weights, each row containes weights for a new epoch
+    - num_inner_updates: The number of updates in the inner loop
+    - epoch_num: The number of the current epoch
+    - inner_lr: The learning rate for the inner loop updates
+    - meta_lr: The learning rate for the meta-update step
+    - num_tasks: The total number of tasks in the taskspace selected
+
+    Returns:
+    - None
+    """
+
+    total_outer_gradients = [tf.zeros_like(weight) for weight in weights[0]]
+    meta_optimizer = tf.train.AdamOptimizer(meta_lr)
+    
+    for task in tasks:
+        # Unpack the task data
+        support_set_dir, query_set_dir = task
+
+        # Run the inner loop to get updated weights
+        updated_weights, _ = maml_inner_loop(support_set_dir, weights, epoch_num, num_inner_updates, inner_lr)
+
+        # Evaluate the model on the query set using the updated weights
+        _, query_set_gradients = vid2depth(query_set_dir, False, updated_weights[0], updated_weights[1], updated_weights[2], 1, epoch_num)
+        
+        for i in range(3):
+            total_outer_gradients[i] = total_outer_gradients[i] + query_set_gradients[i]
+    
+    meta_gradients = [grad / num_tasks for grad in total_outer_gradients]
+
+    # Apply meta-gradients (meta-optimizer step)
+    meta_optimizer.apply_gradients(zip(meta_gradients, weights))
+
+def train_maml():
+    
+    """
+    Train the MAML model.
+
+    Args:
+    - data_dir: Directory where tasks data is stored.
+    - num_tasks: Number of tasks to train on.
+    - num_inner_updates: Number of updates in the inner loop.
+    - inner_lr: Learning rate for the inner loop.
+    - meta_lr: Learning rate for the meta-update.
+    - num_epochs: Number of epochs to train.
+    """
+    weights = [
+    [tf.Variable(def_rec_wt, trainable=True),
+     tf.Variable(def_sm_wt, trainable=True),
+     tf.Variable(def_ssim_wt, trainable=True)]
+]
+
+    for epoch in range(FLAGS.num_epochs):
+        tasks = []
+        # Load tasks for this epoch
+        for task_index in range(FLAGS.num_tasks):
+            task_name = 'task{}'.format(task_index + 1)
+            support_set_dir = os.path.join(FLAGS.data_dir, task_name, 'support')
+            query_set_dir = os.path.join(FLAGS.data_dir, task_name, 'query')
+            tasks.append((support_set_dir, query_set_dir))
+
+        # Perform MAML outer loop
+        maml_outer_loop(tasks, weights, FLAGS.num_inner_updates, epoch, FLAGS.inner_lr, FLAGS.meta_lr, FLAGS.num_tasks)
+
+
+# Learning rates and hyperparameters for the MAML training
+inner_lr = 0.0002
+meta_lr = 0.001
+
+def_rec_wt = 0.85
+def_sm_wt = 0.05
+def_ssim_wt = 0.15
+
+def main(argv):
+    del argv  # Unused.
+    train_maml()
+
+if __name__ == '__main__':
+    app.run(main)
+
---
+++ research/vid2depth/inferenceMaml.py
@@ -0,0 +1,168 @@
+# Copyright 2017 The TensorFlow Authors All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+"""Generates depth estimates for an entire DATASET video."""
+
+# Example usage:
+#
+# python inference.py \
+#   --logtostderr \
+#   --dataset_dir ~/vid2depth/dataset-raw-uncompressed \
+#   --dataset_video 2011_09_26/2011_09_26_drive_0009_sync \
+#   --output_dir ~/vid2depth/inference \
+#   --model_ckpt ~/vid2depth/trained-model/model-119496
+#
+# python inference.py \
+#   --logtostderr \
+#   --dataset_dir ~/vid2depth/dataset-raw-uncompressed \
+#   --dataset_video test_files_eigen \
+#   --output_dir ~/vid2depth/inference \
+#   --model_ckpt ~/vid2depth/trained-model/model-119496
+#
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+from absl import app
+from absl import flags
+from absl import logging
+import matplotlib.pyplot as plt
+import modelMaml as model
+import numpy as np
+import scipy.misc
+import tensorflow as tf
+import util
+import imageio
+from PIL import Image
+
+gfile = tf.gfile
+
+HOME_DIR = os.path.expanduser('~')
+DEFAULT_OUTPUT_DIR = os.path.join(HOME_DIR, 'vid2depth/inference')
+DEFAULT_DATASET_DIR = os.path.join(HOME_DIR, 'dataset-raw-uncompressed')
+
+flags.DEFINE_string('output_dir', DEFAULT_OUTPUT_DIR,
+                    'Directory to store estimated depth maps.')
+flags.DEFINE_string('dataset_dir', DEFAULT_DATASET_DIR, 'DATASET dataset directory.')
+flags.DEFINE_string('model_ckpt', None, 'Model checkpoint to load.')
+flags.DEFINE_string('dataset_video', None, 'DATASET video directory name.')
+flags.DEFINE_integer('batch_size', 4, 'The size of a sample batch.')
+flags.DEFINE_integer('img_height', 128, 'Image height.')
+flags.DEFINE_integer('img_width', 416, 'Image width.')
+flags.DEFINE_integer('seq_length', 3, 'Sequence length for each example.')
+FLAGS = flags.FLAGS
+
+flags.mark_flag_as_required('dataset_video')
+flags.mark_flag_as_required('model_ckpt')
+
+CMAP = 'plasma'
+
+
+def _run_inference():
+  """Runs all images through depth model and saves depth maps."""
+  ckpt_basename = os.path.basename(FLAGS.model_ckpt)
+  ckpt_modelname = os.path.basename(os.path.dirname(FLAGS.model_ckpt))
+  output_dir = os.path.join(FLAGS.output_dir,
+                            FLAGS.dataset_video.replace('/', '_') + '_' +
+                            ckpt_modelname + '_' + ckpt_basename)
+  if not gfile.Exists(output_dir):
+    gfile.MakeDirs(output_dir)
+  inference_model = model.Model(is_training=False,
+                                seq_length=FLAGS.seq_length,
+                                batch_size=FLAGS.batch_size,
+                                img_height=FLAGS.img_height,
+                                img_width=FLAGS.img_width)
+  vars_to_restore = util.get_vars_to_restore(FLAGS.model_ckpt)
+  saver = tf.train.Saver(vars_to_restore)
+  sv = tf.train.Supervisor(logdir='/tmp/', saver=None)
+  with sv.managed_session() as sess:
+    saver.restore(sess, FLAGS.model_ckpt)
+    if FLAGS.dataset_video == 'test_files_eigen':
+      im_files = util.read_text_lines(
+          util.get_resource_path('dataset/dataset/test_files_eigen.txt'))
+      im_files = [os.path.join(FLAGS.dataset_dir, f) for f in im_files]
+    else:
+      video_path = os.path.join(FLAGS.dataset_dir, FLAGS.dataset_video)
+      im_files = gfile.Glob(os.path.join(video_path,'*.jpg'))
+      im_files = [f for f in im_files if 'disp' not in f]
+      im_files = sorted(im_files)
+    for i in range(0, len(im_files), FLAGS.batch_size):
+      if i % 100 == 0:
+        logging.info('Generating from %s: %d/%d', ckpt_basename, i,
+                     len(im_files))
+      inputs = np.zeros(
+          (FLAGS.batch_size, FLAGS.img_height, FLAGS.img_width, 3),
+          dtype=np.uint8)
+      for b in range(FLAGS.batch_size):
+        idx = i + b
+        if idx >= len(im_files):
+          break
+        # im = scipy.misc.imread(im_files[idx])
+        im = imageio.imread(im_files[idx])
+        im_pil = Image.fromarray(im)
+        inputs[b] = np.array(im_pil.resize((FLAGS.img_width, FLAGS.img_height)))
+        # inputs[b] = scipy.misc.imresize(im, (FLAGS.img_height, FLAGS.img_width))
+      results = inference_model.inference(inputs, sess, mode='depth')
+      for b in range(FLAGS.batch_size):
+        idx = i + b
+        if idx >= len(im_files):
+          break
+        if FLAGS.dataset_video == 'test_files_eigen':
+          depth_path = os.path.join(output_dir, '%03d.png' % idx)
+        else:
+          depth_path = os.path.join(output_dir, '%04d.png' % idx)
+        depth_map = results['depth'][b]
+        depth_map = np.squeeze(depth_map)
+        colored_map = _normalize_depth_for_display(depth_map, cmap=CMAP)
+        input_float = inputs[b].astype(np.float32) / 255.0
+        vertical_stack = np.concatenate((input_float, colored_map), axis=0)
+        imageio.imwrite(depth_path, vertical_stack)
+        # scipy.misc.imsave(depth_path, vertical_stack)
+
+
+def _gray2rgb(im, cmap=CMAP):
+  cmap = plt.get_cmap(cmap)
+  rgba_img = cmap(im.astype(np.float32))
+  rgb_img = np.delete(rgba_img, 3, 2)
+  return rgb_img
+
+
+def _normalize_depth_for_display(depth,
+                                 pc=95,
+                                 crop_percent=0,
+                                 normalizer=None,
+                                 cmap=CMAP):
+  """Converts a depth map to an RGB image."""
+  # Convert to disparity.
+  disp = 1.0 / (depth + 1e-6)
+  if normalizer is not None:
+    disp /= normalizer
+  else:
+    disp /= (np.percentile(disp, pc) + 1e-6)
+  disp = np.clip(disp, 0, 1)
+  disp = _gray2rgb(disp, cmap=cmap)
+  keep_h = int(disp.shape[0] * (1 - crop_percent))
+  disp = disp[:keep_h]
+  return disp
+
+
+def main(_):
+  _run_inference()
+
+
+if __name__ == '__main__':
+  app.run(main)
